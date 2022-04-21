4c8286c8a33c115a354a5e75445bd38227605a1c
Om Shahane <71377782+shahane806@users.noreply.github.com>
2022-04-21 12:42:16 +0530

    Update que2.php

--- web/Assignment2/setA/que2.php
+++ web/Assignment2/setA/que2.php
@@ -1,1 +1,15 @@
 #Design a HTML: form to accept a string. Write a php function to reverse a string.
+
+<html>
+	<form method="post" action = "./fact.php">
+	
+	<input type="test" name="str" id="str"/>
+	<input type="submit" name="submit" id="submit">
+	</form>
+</htm>
+<?php
+	$str = $_POST['str'];
+	$str = strrev($str); #by using builtin function
+	
+	echo $str;
+?>
