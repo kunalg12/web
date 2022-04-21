f1ee2e0e1de966af04367631e6ebece3eccd5c4d
Om Shahane <71377782+shahane806@users.noreply.github.com>
2022-04-21 12:54:08 +0530

    Update que2.php

--- web/Assignment2/setA/que2.php
+++ web/Assignment2/setA/que2.php
@@ -1,5 +1,4 @@
 #Design a HTML: form to accept a string. Write a php function to reverse a string.
-
 <html>
 	<form method="post" action = "./fact.php">
 	
@@ -9,7 +8,15 @@
 </htm>
 <?php
 	$str = $_POST['str'];
-	$str = strrev($str); #by using builtin function
+	#$str = strrev($str); #by using builtin function
+	function revstr($str)
+	{
+		$i = 0;
+		for($i = strlen($str)-1;$i>=0;$i--)
+		{
+			 echo $str[$i];
+		}
+	}
+	revstr($str);
 	
-	echo $str;
 ?>
