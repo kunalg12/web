6f34fa88380a413e94c127916e3560aa0ed7df0f
Om Shahane <71377782+shahane806@users.noreply.github.com>
2022-04-22 12:30:28 +0530

    Update que1.php

--- web/Assignment2/setB/que1.php
+++ web/Assignment2/setB/que1.php
@@ -1,1 +1,27 @@
 #Design a html page to accept a number and write a php script to display that number in words i.e. 123 -> one two three
+<html>
+	<form method="post" action = "./fact.php">
+	
+	<input type="str" name="str" id="str"/>
+	<input type="submit" name="submit" id="submit">
+	</form>
+</htm>
+<?php
+	$str = $_POST['str'];
+	
+	$rem=array();
+	$s = ["zero","one","two","three","four","five","six","Seven","eight","nine"];
+	for($i = 0;$str>1;$i++)
+	{
+		$rem[$i] = $str%10;
+		#echo $rem[$i];
+		$str = $str/10;
+	}
+	
+	
+	for($j = $i-1;$j>=0;$j--)
+	{
+		 echo $s[$rem[$j]]." ";
+	}
+	
+?>
