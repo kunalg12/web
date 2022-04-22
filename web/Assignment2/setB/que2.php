a99323dba396805bf38b7f6142daf6063058c534
Om Shahane <71377782+shahane806@users.noreply.github.com>
2022-04-22 12:32:00 +0530

    Create que2.php

---
+++ web/Assignment2/setB/que2.php
@@ -0,0 +1,3 @@
+#Design a html form to accept a string. Write a php script for the following 
+#a. Write a function to count the total number of Vowels from the script
+#b. Show the occurrences of each Vowel from the script
