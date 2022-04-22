663a6dfab99d74c2ae48132bd2d818da12cf3419
Om Shahane <71377782+shahane806@users.noreply.github.com>
2022-04-22 12:45:51 +0530

    Create que3.php

---
+++ web/Assignment2/setB/que3.php
@@ -0,0 +1,6 @@
+#Write a php script for the following 
+#a.Design a form to accept two numbers from the users.
+#b. Give option to choose an arithmetic operation
+#(use Radio Button) 
+#c. Display the result on next form
+#d. Use concept of default parameter
