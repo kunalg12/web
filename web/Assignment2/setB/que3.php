c07f8a5f1a348892dcf2448707eec41e323a715b
Om Shahane <71377782+shahane806@users.noreply.github.com>
2022-04-28 11:51:54 +0530

    Update que3.php

--- web/Assignment2/setB/que3.php
+++ web/Assignment2/setB/que3.php
@@ -4,3 +4,50 @@
 #(use Radio Button) 
 #c. Display the result on next form
 #d. Use concept of default parameter
+
+<html>
+	<form method="post" action = "./fact.php">
+	
+	<input type="number" name="num1" id="num1"/>
+	<input type="number" name="num2" id="num2"/>
+	+<input type="radio" name = "ope"    value = "+"/>
+	-<input type="radio" name = "ope"	value = "-"/>
+	*<input type="radio" name = "ope"	value = "*"/>
+	/<input type="radio" name = "ope"	value = "/"/>
+	
+	<input type="submit" name="submit" id="submit">
+	</form>
+</htm>
+<?php
+ 
+	
+		
+	 $num1  = $_POST['num1'];
+	 $num2  = $_POST['num2'];
+	 $ope   = $_POST['ope'];
+	 
+	 if($ope == "+")
+	 {
+		 echo $num1 + $num2;
+	 }
+	 else if ($ope == "-")
+	 {
+		echo $num1 - $num2;
+	 }
+	 else if($ope == "/")
+		 
+		 {
+			 echo $num1 / $num2;
+		 }
+		 
+		 else if ($ope == "*")
+		 {
+			 echo $num1 * $num2;
+		 }
+		 
+	 
+	
+	
+	
+	
+?>
