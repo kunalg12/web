9c473787661826786c25efa4492c025686fbf19c
Om Shahane <71377782+shahane806@users.noreply.github.com>
2022-05-12 06:18:11 +0530

    Delete web directory

--- web/clg/Assignment 1/setA/que1.php
+++
@@ -1,7 +0,0 @@
-<html><h1>1. Write a PHP script to get the PHP version and configuration information</h1></html>
-<?php
-
-        
-        echo phpinfo();
-
-?>
--- web/clg/Assignment 1/setC/que2.php
+++
@@ -1,8 +0,0 @@
-<html>
-    <h1>
-        2. Write a PHP script to display source code of a webpage
-    </h1>
-</html>
-<?php
-
-?>
